566e827c65aa0299e981b7d41a08e93210459886
IbrahimBannan <162644541+IbrahimBannan@users.noreply.github.com>
2024-12-21 16:21:42 +0300

    RockPaperScissors

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a Rock-Paper-Scissors game that can be played on the same pc/laptop or with a multiple pc/laptops
+It's one of my first projects.
+Hope you enjoy it.
---
+++ client.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include "game.h"
+
+int main() {
+    int client_fd;
+    struct sockaddr_in server_addr;
+    char choice;
+    int result;
+    char name[50];
+
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection to server failed");
+        close(client_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connected to the server.\n");
+
+    char buffer[35];
+    recv(client_fd, buffer, sizeof(buffer), 0);
+    printf("%s", buffer);
+    scanf(" %49s", name);
+    send(client_fd, name, strlen(name) + 1, 0);
+
+    do {
+        printf("Enter your choice (R for Rock, P for Paper, S for Scissors): ");
+        scanf(" %c", &choice);
+        while (choice != 'R' && choice != 'P' && choice != 'S') {
+            printf("Invalid choice! Please enter 'R' for Rock, 'P' for Paper, or 'S' for Scissors: ");
+            scanf(" %c", &choice);
+        }
+
+        if (send(client_fd, &choice, sizeof(choice), 0) == -1) {
+            perror("Failed to send choice to server");
+            close(client_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        if (recv(client_fd, &result, sizeof(result), 0) <= 0) {
+            perror("Failed to receive result from server");
+            close(client_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        if (result == WIN) {
+            printf("You win!\n");
+        } else if (result == LOSE) {
+            printf("You lose!\n");
+        } else {
+            printf("It's a draw!\n");
+        }
+
+        char play_again_prompt[50];
+        recv(client_fd, play_again_prompt, sizeof(play_again_prompt), 0);
+        printf("%s", play_again_prompt);
+        char response;
+        scanf(" %c", &response);
+        send(client_fd, &response, sizeof(response), 0);
+
+        if (response != 'Y' && response != 'y') {
+            printf("Exiting the game. Goodbye!\n");
+            break;
+        }
+
+    } while (1);
+
+    close(client_fd);
+    return 0;
+}
